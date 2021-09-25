87b6f0065c05a68134ade93491d7537fb5f14471
Tilmann <tilmann.moeller@gmx.de>
2021-09-25 22:23:57 +0200

    xtopien test

---
+++ xtopien/data.js
@@ -0,0 +1,146 @@
+let data=[
+    {
+        "descr": "Klimacamp #1",
+        "thumb": "alleindiestadt/pics/01a.jpeg",
+        "overview": "alleindiestadt/pics/01b.jpeg",
+        "img": "alleindiestadt/pics/01d.jpeg, alleindiestadt/pics/01e.jpeg, alleindiestadt/pics/01c.jpeg",
+        "audio": "alleindiestadt/podcast/s01e01.mp3",
+        "spz": "Huhn, Kiefer, Spitzmaus"
+    },
+    {
+        "descr": "Klimacamp #2",
+        "thumb": "alleindiestadt/pics/02a.jpeg",
+        "overview": "alleindiestadt/pics/02b.jpeg",
+        "img": "alleindiestadt/pics/02c.jpeg, alleindiestadt/pics/02d.jpeg, alleindiestadt/pics/02e.jpeg, alleindiestadt/pics/02f.jpeg",
+        "audio": "alleindiestadt/podcast/s01e02.mp3",
+        "spz": "Erdkröte, Efeu, Adlerfarn, Admiral"
+    },
+    {
+        "descr": "Klimacamp #3",
+        "thumb": "alleindiestadt/pics/03a.jpeg",
+        "overview": "alleindiestadt/pics/03b.jpeg",
+        "img": "alleindiestadt/pics/03c.jpeg, alleindiestadt/pics/03d.jpeg",
+        "audio": "alleindiestadt/podcast/s01e03.mp3",
+        "spz": "Gartenkreuzspinne, Biber"
+    },
+    {
+        "descr": "Klimacamp #4",
+        "thumb": "alleindiestadt/pics/04a.jpeg",
+        "overview": "alleindiestadt/pics/04b.jpeg",
+        "img": "alleindiestadt/pics/04d.jpeg, alleindiestadt/pics/04e.jpeg, alleindiestadt/pics/04c.jpeg",
+        "audio": "alleindiestadt/podcast/s01e04.mp3",
+        "spz": "Kaninchen, Regenwurm, Gemeine Stechmücke"
+    },
+    {
+        "descr": "Klimacamp #5",
+        "thumb": "alleindiestadt/pics/05a.jpeg",
+        "overview": "alleindiestadt/pics/05b.jpeg",
+        "img": "alleindiestadt/pics/05c.jpeg, alleindiestadt/pics/05e.jpeg, alleindiestadt/pics/05d.jpeg",
+        "audio": "alleindiestadt/podcast/s01e05.mp3",
+        "spz": "Schlingnatter, Schlupfwespe, Gartenkreuzspinne"
+    },
+    {
+        "descr": "Umweltfest #1",
+        "thumb": "alleindiestadt/pics/06a.jpeg",
+        "overview": "alleindiestadt/pics/06a.jpeg",
+        "img": "alleindiestadt/pics/06c.jpeg, alleindiestadt/pics/06b.jpeg, alleindiestadt/pics/06d.jpeg",
+        "audio": "alleindiestadt/podcast/s02e01.mp3",
+        "spz": "Rote Waldameise, Zauneidechse, Erdkröte"
+    },
+    {
+        "descr": "Umweltfest #2",
+        "thumb": "alleindiestadt/pics/07a.jpeg",
+        "overview": "alleindiestadt/pics/07b.jpeg",
+        "img": "alleindiestadt/pics/07d.jpeg, alleindiestadt/pics/07c.jpeg",
+        "audio": "alleindiestadt/podcast/s02e02.mp3",
+        "spz": "Turmfalke, Regenwurm, Spitzmaus"
+    },
+    {
+        "descr": "Umweltfest #3",
+        "thumb": "alleindiestadt/pics/08a.jpeg",
+        "overview": "alleindiestadt/pics/08b.jpeg",
+        "img": "alleindiestadt/pics/08c.jpeg, alleindiestadt/pics/08d.jpeg, alleindiestadt/pics/08e.jpeg",
+        "audio": "alleindiestadt/podcast/s02e03.mp3",
+        "spz": "Borkenkäfer, Schlupfwespe, Huhn"
+    },
+    {
+        "descr": "Umweltfest #4",
+        "thumb": "alleindiestadt/pics/09a.jpeg",
+        "overview": "alleindiestadt/pics/09b.jpeg",
+        "img": "alleindiestadt/pics/09c.jpeg",
+        "audio": "alleindiestadt/podcast/s02e04.mp3",
+        "spz": "Reh, Rotfuchs, Biber"
+    },
+    {
+        "descr": "Umweltfest #5",
+        "thumb": "alleindiestadt/pics/10a.jpeg",
+        "overview": "alleindiestadt/pics/10b.jpeg",
+        "img": "alleindiestadt/pics/10c.jpeg, alleindiestadt/pics/10e.jpeg, alleindiestadt/pics/10d.jpeg",
+        "audio": "alleindiestadt/podcast/s02e05.mp3",
+        "spz": "Kaninchen, Huhn, Regenwurm"
+    },
+    {
+        "descr": "Umweltfest #6",
+        "thumb": "alleindiestadt/pics/11a.jpeg",
+        "overview": "alleindiestadt/pics/11b.jpeg",
+        "img": "alleindiestadt/pics/11f.jpeg, alleindiestadt/pics/11d.jpeg, alleindiestadt/pics/11e.jpeg, alleindiestadt/pics/11c.jpeg",
+        "audio": "alleindiestadt/podcast/s02e06.mp3",
+        "spz": "Turmfalke, Schnatterente, Zwergfledermaus, Reh"
+    },
+    {
+        "descr": "Umweltfest #7",
+        "thumb": "alleindiestadt/pics/12a.jpeg",
+        "overview": "alleindiestadt/pics/12b.jpeg",
+        "img": "alleindiestadt/pics/12e.jpeg, alleindiestadt/pics/12c.jpeg, alleindiestadt/pics/12d.jpeg",
+        "audio": "alleindiestadt/podcast/s02e07.mp3",
+        "spz": "Zwergfledermaus, Schlingnatter, Gartenkreuzspinne, Reh"
+    },
+    {
+        "descr": "Umweltfest #8",
+        "thumb": "alleindiestadt/pics/13a.jpeg",
+        "overview": "alleindiestadt/pics/13b.jpeg",
+        "img": "alleindiestadt/pics/13c.jpeg, alleindiestadt/pics/13d.jpeg",
+        "audio": "alleindiestadt/podcast/s02e08.mp3",
+        "spz": "Biber, Schlingnatter"
+    },
+    {
+        "descr": "Umweltfest #9",
+        "thumb": "alleindiestadt/pics/14a.jpeg",
+        "overview": "alleindiestadt/pics/14b.jpeg",
+        "img": "alleindiestadt/pics/14d.jpeg, alleindiestadt/pics/14c.jpeg, alleindiestadt/pics/14e.jpeg",
+        "audio": "alleindiestadt/podcast/s02e09.mp3",
+        "spz": "Erdkröte, Zauneidechse, Rote Waldameise"
+    },
+    {
+        "descr": "Umweltfest #10",
+        "thumb": "alleindiestadt/pics/15a.jpeg",
+        "overview": "alleindiestadt/pics/15b.jpeg",
+        "img": "alleindiestadt/pics/15c.jpeg, alleindiestadt/pics/15d.jpeg, alleindiestadt/pics/15e.jpeg",
+        "audio": "alleindiestadt/podcast/s02e10.mp3",
+        "spz": "Nachtigall, Katze"
+    },
+    {
+        "descr": "Umweltfest #11",
+        "thumb": "alleindiestadt/pics/16a.jpeg",
+        "overview": "alleindiestadt/pics/16b.jpeg",
+        "img": "alleindiestadt/pics/16c.jpeg, alleindiestadt/pics/16e.jpeg, alleindiestadt/pics/16d.jpeg",
+        "audio": "alleindiestadt/podcast/s02e11.mp3",
+        "spz": "Kaninchen, Rotfuchs, Schnatterente"
+    },
+    {
+        "descr": "Umweltfest #12",
+        "thumb": "alleindiestadt/pics/17a.jpeg",
+        "overview": "alleindiestadt/pics/17b.jpeg",
+        "img": "alleindiestadt/pics/17d.jpeg, alleindiestadt/pics/17c.jpeg, alleindiestadt/pics/17e.jpeg",
+        "audio": "alleindiestadt/podcast/s02e12.mp3",
+        "spz": "Reh, Gartenkreuzspinne, Rotkehlchen"
+    },
+    {
+        "descr": "Umweltfest #13",
+        "thumb": "alleindiestadt/pics/18a.jpeg",
+        "overview": "alleindiestadt/pics/18b.jpeg",
+        "img": "alleindiestadt/pics/18f.jpeg, alleindiestadt/pics/18d.jpeg, alleindiestadt/pics/18c.jpeg, alleindiestadt/pics/18e.jpeg",
+        "audio": "alleindiestadt/podcast/s02e13.mp3",
+        "spz": "Schlingnatter, Spitzmaus, Nachtigall, Admiral"
+    }
+]
---
+++ xtopien/index.js
@@ -0,0 +1,65 @@
+let counter=0;
+window.onload = function() {
+    for (let i=0; i<data.length;i++){
+        document.getElementById("stadt_container").innerHTML=document.getElementById("stadt_container").innerHTML + "<div id=\"" + "stadt_" + i + "\"style=\"background-image: url(\'"+ data[i].thumb + "\');\" onClick=\"changeCity(" + i +",1)\"><span class=\"stadt_container_text\">" + data[i].descr + "</span></div>"
+    }
+    document.getElementById("stadt_img")
+    changeCity(0,0);
+}
+
+function changeCity(id,s){
+    counter=0;
+    document.getElementById("stadt_img").style.backgroundImage = "url('" + data[id].overview +"')";
+    document.getElementById("stadt_audio").src=data[id].audio;
+    document.getElementById("stadt_titel").innerHTML=data[id].descr;
+    let spz=data[id].spz.split(", ");
+    document.getElementById("stadt_spz_pics").innerHTML="";
+    for (let i=0; i<spz.length;i++){
+        document.getElementById("stadt_spz_pics").innerHTML=document.getElementById("stadt_spz_pics").innerHTML+ "<img id=\"spz_pics_" + i + "\" src=\"alleindiestadt/spz/" + spz[i] + ".png\" class=\"spz_img\" onClick=\"stadtImg2(" + i + "," + id + ")\"></img><span id=\"stadt_spz_" + i + "\" class=\"textcaps\" style=\"cursor: pointer;\" onClick=\"stadtImg2(" + i + "," + id + ")\">" + spz[i] + "</span><br>"
+    }
+    document.getElementById("stadt_prev").setAttribute("onclick", "javascript: stadtImg(-1," + id + ");");
+    document.getElementById("stadt_next").setAttribute("onclick", "javascript: stadtImg(1," + id + ");");
+    if(s==1){
+        window.scrollBy({top: document.getElementById("stadt_content").getBoundingClientRect().top, left: 0, behavior: 'smooth'});
+    }
+}
+
+
+function stadtImg(c, id){
+    counter=counter+c;
+    let img=data[id].img.split(", ");
+    img.unshift(data[id].overview);
+    if (counter>=img.length){
+        counter=0;
+    }
+    if (counter<0){
+        counter=img.length-1;
+    }
+    document.getElementById("stadt_img").style.backgroundImage = "url('" + img[counter] +"')";
+    setActiveSpz(counter-1, id)
+}
+
+function stadtImg2(c, id){
+    let img=data[id].img.split(", ");
+    img.unshift(data[id].overview);
+    counter=c+1;
+    document.getElementById("stadt_img").style.backgroundImage = "url('" + img[c+1] +"')";
+    setActiveSpz(c, id)
+}
+
+function setActiveSpz(c, id){
+    let spz=data[id].spz.split(", ");
+    if(c==-1){
+        for(let i=0; i<spz.length;i++){
+            document.getElementById("spz_pics_" + i).style.filter="saturate(100%)";
+            document.getElementById("stadt_spz_" + i).style.borderBottom="0px";
+        }
+    } else {
+        for(let i=0; i<spz.length;i++){
+            document.getElementById("spz_pics_" + i).style.filter="saturate(0%)";
+            document.getElementById("stadt_spz_" + i).style.borderBottom="0px";
+        }
+        document.getElementById("spz_pics_" + c).style.filter="saturate(100%)";
+        document.getElementById("stadt_spz_" + c).style.borderBottom="6px solid #F5FF02";
+    }
+}
