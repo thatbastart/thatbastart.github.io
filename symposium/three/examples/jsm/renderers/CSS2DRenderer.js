c294053a1f66f2adeaecce7b4ebdfd0644b1b041
Tilmann <tilmann.moeller@gmx.de>
2021-11-18 00:25:38 +0100

    symposium css wip

--- symposium/three/examples/jsm/renderers/CSS2DRenderer.js
+++ symposium/three/examples/jsm/renderers/CSS2DRenderer.js
@@ -122,11 +122,11 @@ class CSS2DRenderer {
 				if ( /apple/i.test( navigator.vendor ) ) {
 
 					// https://github.com/mrdoob/three.js/issues/21415
-					element.style.transform = 'translate(-50%,-50%) translate(' + Math.round( _vector.x * _widthHalf + _widthHalf ) + 'px,' + Math.round( - _vector.y * _heightHalf + _heightHalf ) + 'px)';
+					element.style.transform = 'translate(-50%,-100%) translate(' + Math.round( _vector.x * _widthHalf + _widthHalf ) + 'px,' + Math.round( - _vector.y * _heightHalf + _heightHalf ) + 'px)';
 
 				} else {
 
-					element.style.transform = 'translate(-50%,-50%) translate(' + ( _vector.x * _widthHalf + _widthHalf ) + 'px,' + ( - _vector.y * _heightHalf + _heightHalf ) + 'px)';
+					element.style.transform = 'translate(-50%,-100%) translate(' + ( _vector.x * _widthHalf + _widthHalf ) + 'px,' + ( - _vector.y * _heightHalf + _heightHalf ) + 'px)';
 
 				}
 
