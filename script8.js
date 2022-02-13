92ccac6f6c119544a6cc5a2d2ebf9d865ee7fd1b
Tilmann <tilmann.moeller@gmx.de>
2022-02-13 04:09:18 +0100

    script8

---
+++ script8.js
@@ -0,0 +1,12 @@
+<script>
+  window.addEventListener("load", function(event) {
+    document.getElementsByClassName("c-images__letterbox")[0].style.padding="0% 10% 0% 10%";
+document.getElementsByClassName("c-images__letterbox")[0].style.background="transparent";
+document.getElementsByClassName("c-images__letterbox")[1].style.padding="0% 10% 0% 10%";
+document.getElementsByClassName("c-images__letterbox")[1].style.background="transparent";
+document.getElementsByClassName("c-images__letterbox")[2].style.padding="0% 10% 0% 10%";
+document.getElementsByClassName("c-images__letterbox")[2].style.background="transparent";
+document.getElementsByClassName("c-images__letterbox")[3].style.padding="0% 10% 0% 10%";
+document.getElementsByClassName("c-images__letterbox")[3].style.background="transparent";
+  });
+</script>
