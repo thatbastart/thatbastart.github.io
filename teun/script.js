d50d2c869ecdee14e70157835a1f4d619e9063a9
Tilmann <tilmann.moeller@gmx.de>
2022-02-16 16:12:12 +0100

    teun first test

---
+++ teun/script.js
@@ -0,0 +1,7 @@
+function link(){
+    if(document.getElementById("link1").style.visibility=="hidden"){
+        document.getElementById("link1").style.visibility="visible";
+    } else {
+        document.getElementById("link1").style.visibility="hidden";
+    }
+}
