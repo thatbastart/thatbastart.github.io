95e165bba151dffa7a3ae9bc690ea7d32aa7abbc
Tilmann <tilmann.moeller@gmx.de>
2022-03-02 20:08:15 +0100

    landscapes vine

--- landscapes/script.js
+++ landscapes/script.js
@@ -98,6 +98,7 @@ function init() {
             let texture = new THREE.TextureLoader().load("vine.png");
             texture.wrapS=THREE.RepeatWrapping;
             texture.wrapT=THREE.RepeatWrapping;
+            texture.repeat.set(0.5,0.5);
             let vineMaterial = new THREE.MeshBasicMaterial( { map: texture, transparent: true} );
             vine.material=vineMaterial;
             console.log(vine);
