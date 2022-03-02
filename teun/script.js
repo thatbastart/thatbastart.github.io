a2938c888cc0aeb2b90623244f448741a66ac1f0
Tilmann <tilmann.moeller@gmx.de>
2022-03-02 23:17:15 +0100

    teun

--- teun/script.js
+++
@@ -1,7 +0,0 @@
-function link(){
-    if(document.getElementById("link1").style.visibility=="hidden"){
-        document.getElementById("link1").style.visibility="visible";
-    } else {
-        document.getElementById("link1").style.visibility="hidden";
-    }
-}
