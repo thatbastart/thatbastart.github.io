002da7869b02139cd7800c79992ae3d33e665027
Tilmann <tilmann.moeller@gmx.de>
2022-03-05 00:49:30 +0100

    landscapes tree text

--- landscapes/script.js
+++ landscapes/script.js
@@ -111,13 +111,14 @@ function init() {
                     this.txt.position.set(-center.x,1,0.5);
                     break;
                 case 1:
-                    this.txt.position.set(1,-0.4,0.5);
+                    this.txt.position.set(1,0,0.5);
                     break;
                 case 2:
-                    this.txt.position.set(-center.x,-1,0.5);
+                    this.txt.position.set(-center.x,-1.5,0.5);
                     break;
                 case 3:
-
+                    let bBox=this.txt.geometry.boundingBox;
+                    this.txt.position.set(-(Math.abs(bBox.min.x)+bBox.max.x)-1,0,0.5);
                     break;
             }
             console.log(this.txt);
