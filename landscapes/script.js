8024749232c121a4090ef1df2ff18db943772a5d
Tilmann <tilmann.moeller@gmx.de>
2022-03-05 02:12:49 +0100

    landscapes tree rotation

--- landscapes/script.js
+++ landscapes/script.js
@@ -215,7 +215,7 @@ function render() {
     //vj_tree.rotation.set(0,camera.rotation.y,0);
     
     //console.log(vj_tree.rotation.y*(180/Math.PI));
-    vj_tree.quaternion.copy(camera.quaternion);
+    vj_tree.quaternion.set(0,camera.quaternion.y,0,0);
     console.log(vj_tree.quaternion);
     renderer.render(scene, camera);
 }
