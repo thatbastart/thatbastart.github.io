f263d2fcb7a045f6874173d22054a03224f9fe05
Tilmann <tilmann.moeller@gmx.de>
2022-03-06 19:11:02 +0100

    landscapes multiline text align

--- landscapes/script.js
+++ landscapes/script.js
@@ -129,15 +129,36 @@ function init() {
         txtAlign(){
             let lines=this.title.split("\n");
             let linesGeo=[];
+            let linesMesh=[];
+            let longestLine=0;
             for(let i=0;i<lines.length;i++){
                 let geo=new THREE.TextGeometry(lines[i], {font: font, size: 0.8, height: 0, curveSegments: 8} );
-                let mesh=new THREE.Mesh(geo);
-                mesh.position.set(0,-(i+1)*1.6,0);
-                mesh.updateMatrix();
-                mesh.geometry.applyMatrix4(mesh.matrix);
-                linesGeo[i]=mesh.geometry;
-
+                linesMesh[i]=new THREE.Mesh(geo);
+                linesMesh[i].position.set(0,-(i+1)*1.6,0);
+                linesMesh[i].updateMatrix();
+                linesMesh[i].geometry.applyMatrix4(linesMesh[i].matrix);
+                linesMesh[i].geometry.computeBoundingBox();
+                
+                if(i>0){
+                    let bBox=linesMesh[i].geometry.boundingBox;
+                    let bBoxPrev=linesMesh[i-1].geometry.boundingBox;
+                    if(bBox.max.x - bBox.min.x > bBoxPrev.max.x - bBoxPrev.min.x){
+                        longestLine=i;
+                    }
+                }
             }
+
+            if(this.pos==0 || this.pos==2){
+                for(let i=0;i<lines.length;i++){
+                    let offset=linesMesh[longestLine].geometry.getCenter(new THREE.Vector3()) - linesMesh[i].geometry.getCenter(new THREE.Vector3());
+                    linesMesh[i].position.set(0,offset,0);
+                    linesMesh[i].updateMatrix();
+                    linesMesh[i].geometry.applyMatrix4(linesMesh[i].matrix);
+
+                    linesGeo[i]=linesMesh[i].geometry;
+                }
+            }
+
             
             return BufferGeometryUtils.mergeBufferGeometries(linesGeo, false);
         }
