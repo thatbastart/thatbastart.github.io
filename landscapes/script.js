1ca7afc0b8c973c7672efc53f6fdb4c30b72cad2
Tilmann <tilmann.moeller@gmx.de>
2022-03-12 01:31:55 +0100

    landscapes treepoints stories

--- landscapes/script.js
+++ landscapes/script.js
@@ -277,10 +277,12 @@ function onMouseMove( event ) {
     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
     raycaster.setFromCamera( mouse, camera );
     for(let i=0; i<vj_treepoints.length; i++){
-        let inverseMatrix = new THREE.Matrix4();
-        let ray = new THREE.Ray();
-        inverseMatrix.copy(vj_treepoints[i].txt.matrixWorld).invert();
-        ray.copy(raycaster.ray).applyMatrix4(inverseMatrix);
+        if(vj_treepoints[i].txt!=undefined){
+            let inverseMatrix = new THREE.Matrix4();
+            let ray = new THREE.Ray();
+            inverseMatrix.copy(vj_treepoints[i].txt.matrixWorld).invert();
+            ray.copy(raycaster.ray).applyMatrix4(inverseMatrix);
+        }
         if(raycaster.intersectObject(vj_treepoints[i].sph).length==1 || ray.intersectsBox(vj_treepoints[i].txt.geometry.boundingBox) == true){ // pointer down over sphere
             document.body.style.cursor="pointer";
             if(vj_treepoints[i].pos==-1){
